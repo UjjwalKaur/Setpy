4e0fc4c59ff998b678818cc51c97f83012bcd932
UjjwalKaur <75835619+UjjwalKaur@users.noreply.github.com>
2022-05-26 13:31:55 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,10 @@
+firstSet = {1,4,8,9,3}
+secondSet = {7,5,8,3,2}
+#union
+print(firstSet|secondSet)
+#intersection
+print(firstSet&secondSet)
+#difference
+print(firstSet-secondSet)
+#symmetric difference
+print(firstSet^secondSet)
